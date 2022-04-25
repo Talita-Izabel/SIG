e0aab4dcacb98caf50c9dad206b2e3837fb6bd58
lucasrmts2 <lucasrmts2@gmail.com>
2022-04-24 21:00:25 -0300

    feat: add functions to select menu item and change form title

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
-function setLoginTitle() {
+/**
+ * @description Função que altera o título do login conforme o item selecionado
+ * @param {*} titleSelected item do menu selecionado (SIGAA, SIPAC, SIGRH, SIGPP, SIGED, SIGAdmin)
+ */
+function setLoginTitle(titleSelected) {
   var title = document.getElementsByClassName("title");
-  const navItens = document.getElementsByClassName("nav-item");
-  console.log(navItens);
-  if (title) {
-    title.innerHTML = "Login";
-  }
+  title[0].innerHTML = titleSelected;
 }
 
+/**
+ * @description Função que remove a classe item-selected dos itens do menu que não seja o item selecionado
+ * @param {*} exceptionItem item do menu selecionado que não terá o estilo excluído (SIGAA, SIPAC, SIGRH, SIGPP, SIGED, SIGAdmin)
+ */
+function removeOtherSelectedItems(exceptionItem) {
+  const navItens = [];
+  exceptionItem !== "SIGAA" &&
+    navItens.push(document.getElementsByName("SIGAA"));
+  exceptionItem !== "SIPAC" &&
+    navItens.push(document.getElementsByName("SIPAC"));
+  exceptionItem !== "SIGRH" &&
+    navItens.push(document.getElementsByName("SIGRH"));
+  exceptionItem !== "SIGPP" &&
+    navItens.push(document.getElementsByName("SIGPP"));
+  exceptionItem !== "SIGED" &&
+    navItens.push(document.getElementsByName("SIGED"));
+  exceptionItem !== "SIGAdmin" &&
+    navItens.push(document.getElementsByName("SIGAdmin"));
+
+  navItens.forEach((item) => {
+    item[0].classList.remove("item-selected"); // Removendo a classe do span
+    item[1].classList.remove("item-selected"); // Removendo a classe do icon
+  });
+}
+
+/**
+ * @description Função que adiciona a classe item-selected ao item selecionado
+ * @param {*} selected item do menu selecionado (SIGAA, SIPAC, SIGRH, SIGPP, SIGED, SIGAdmin)
+ */
 function selectItem(selected) {
   const itemSelected = document.getElementsByName(selected);
-  console.log("SELECIIONADO", itemSelected);
+  itemSelected[0].classList.contains("item-selected")
+    ? itemSelected[0].classList.remove("item-selected")
+    : itemSelected[0].classList.add("item-selected");
+  itemSelected[1].classList.contains("item-selected")
+    ? itemSelected[1].classList.remove("item-selected")
+    : itemSelected[1].classList.add("item-selected");
+  removeOtherSelectedItems(selected);
+  setLoginTitle(selected);
 }
